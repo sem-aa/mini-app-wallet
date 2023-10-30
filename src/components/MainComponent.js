7cdb5996b1812853c7c1fca78aec72202a3487f9
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-10-30 02:36:44 +0200

    add some fn from sdk telegram

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,62 @@
-import { useSDK } from "@tma.js/sdk-react";
+import { useEffect, useState } from "react";
+import { useMainButton, useBackButton, useInitData } from "@tma.js/sdk-react";
+import style from "./MainComponent.module.css";
 
 export function MainComponent() {
-  const sdk = useSDK();
-  console.log(sdk);
+  const [data, setData] = useState();
+  const [count, setCount] = useState(0);
 
-  return <p>Hello wallet</p>;
+  const initData = useInitData();
+  const mainButton = useMainButton();
+  const backButton = useBackButton();
+
+  useEffect(() => {
+    const onMainButtonClick = () => setCount((prevCount) => prevCount + 1);
+    const onBackButtonClick = () => setCount((prevCount) => prevCount - 1);
+
+    mainButton.enable().show();
+    mainButton.on("click", onMainButtonClick);
+    backButton.on("click", onBackButtonClick);
+
+    return () => {
+      mainButton.off("click", onMainButtonClick);
+      mainButton.hide();
+      backButton.off("click", onBackButtonClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    mainButton.setText(`Count is ${count}`);
+  }, [mainButton, count]);
+
+  useEffect(() => {
+    if (count === 0) {
+      backButton.hide();
+      return;
+    }
+    backButton.show();
+  }, [backButton, count]);
+
+  useEffect(() => {
+    if (initData) {
+      setData(initData);
+    }
+  }, [initData]);
+
+  return (
+    <div className={style.mainComponent}>
+      <h1>
+        Hello {data?.user.firstName} {data?.user.lastName}
+      </h1>
+      <div className={style.userInfo}>
+        <p className={style.userInfoName}>
+          Here is some information that I have about you:
+        </p>
+        <p className={style.userData}>your hash - {data?.hash}</p>
+        <p className={style.userData}>your id - {data?.user.id}</p>
+        <p className={style.userData}>your username - {data?.user.username}</p>
+      </div>
+    </div>
+  );
 }
